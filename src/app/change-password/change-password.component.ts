367e28a3192c88b0f8dff49255f667af8c1cd1b4
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-07-09 09:01:37 -0400

    change-password confirm password matching check

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -12,34 +12,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ChangePasswordComponent  {
 
-  form: FormGroup;
+  changepasswordform: FormGroup;
 
   constructor(fb:FormBuilder) { 
 
-  this.form=fb.group({
+  this.changepasswordform=fb.group({
     oldPassWord:[
       '', 
       Validators.required,
       PassWordValidators.invalidOldPassword],
     newPassWord:['', Validators.required],
-    confirmPassWord:['', 
-        Validators.required,
-        
-    ],
-
+    confirmPassWord:['', Validators.required]
+  },{
+    validator: PassWordValidators.isMatchPassword
   });
   }
 
   get oldPassWord(){
-    return this.form.get('oldPassWord');
+    return this.changepasswordform.get('oldPassWord');
   }
 
   get newPassWord(){
-    return this.form.get('newPassWord');
+    return this.changepasswordform.get('newPassWord');
   }
   
   get confirmPassWord(){
-    return this.form.get('confirmPassWord');
+    return this.changepasswordform.get('confirmPassWord');
   }
 
 }
--- src/app/change-password/password.validators.ts
+++ src/app/change-password/password.validators.ts
@@ -5,6 +5,7 @@ export class PassWordValidators{
     static invalidOldPassword(control: AbstractControl) :Promise<ValidationErrors | null>{
         return new Promise((resolve, reject)=>{
             setTimeout(()=>{
+                // console.log(control);
 
                 if(control.value!=='1234'){
                     resolve({invalidOldPassword: true});
@@ -14,6 +15,19 @@ export class PassWordValidators{
                 }
             }, 2000);
         });
+    }
+
+    static isMatchPassword(control: AbstractControl){
+       
+                let newPassword= control.get('newPassWord');
+                let confirmPassword= control.get('confirmPassWord');
+
+                 console.log(control);
+                if(newPassword?.value!== confirmPassword?.value){
+                    return {isMatchPassword: true};
+                }
+                return null;
+           
     }
     
 }
